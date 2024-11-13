d1a4c0de8b01761a44eca3fde3a5b650f763b701
Mitoz08 <timong201311@yahoo.com>
2024-11-13 18:44:38 +0800

    Added 1. JavaDoc until Pharmacy loadFile()

--- src/DepartmentObject/Pharmacy.java
+++ src/DepartmentObject/Pharmacy.java
@@ -10,12 +10,29 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
-
+/**
+ * The {@code Pharmacy} class manages the medicine stock for the Hospital Management System
+ * <p>
+ *     An instance of this class works off of one database (one text file), allowing
+ *     the Hospital Management System to include multiple pharmacy department if needed.
+ * </p>
+ * <p>
+ *     This class uses the {@code MedicineData} class to store information for each medicine in the hospital
+ * </p>
+ * <p>
+ *     {@code MedicineRequest} and {@code RestockRequest} classes are used to to keep track of the transaction
+ *     going on within the {@code Pharmacy}
+ * </p>
+ *
+ * @see MedicineData    MedicineData - Data type to store medicine details
+ * @see MedicineRequest MedicineRequest - Data type to store medicine request details
+ * @see RestockRequest  RestockRequest - Data type to store medicine restock details
+ */
 public class Pharmacy {
 
     // Attributes
     private HashMap<String, Integer> nameToID;
-    HashMap<Integer,Integer> pendingAmount;
+    private HashMap<Integer,Integer> pendingAmount;
     private HashMap<Integer, MedicineData> medicineStorage;
     private ArrayList<MedicineRequest> medicineRequests;
     private ArrayList<RestockRequest> restockRequests;
@@ -25,6 +42,9 @@ public class Pharmacy {
 
     // Methods
 
+    /**
+     * Constructs a {@code Pharmacy object} with its {@code ArrayList} and {@code HashMap} initialised
+     */
     public Pharmacy(){
         this.nameToID = new HashMap<String,Integer>();
         this.pendingAmount = new HashMap<Integer,Integer>();
@@ -39,30 +59,35 @@ public class Pharmacy {
     }
 
     // Getter/Setter
+
+    /**
+     * Gets a copy of the MedicineStorage
+     * @return a copy of the MedicineStorage
+     */
     public HashMap<Integer,MedicineData> getMedicine() {
         HashMap<Integer,MedicineData> temp = (HashMap<Integer,MedicineData>) this.medicineStorage.clone();
         return temp;
     }
+
+    /**
+     * Sets the file for the {@code Pharmacy} instance to read from
+     * @param fileName file to read from
+     */
     public void setPharmacyFileName(String fileName) {
         this.pharmacyFileName = fileName;
     }
 
+    /**
+     * Terminates the {@code Pharmacy} after saving the data into the file
+     */
     public void endPharmacy() {
         saveFile();
     }
 
+    /**
+     * A method used to test run the program by initialising the default medicine
+     */
     public void testRun() {
-        MedicineData m1 = new MedicineData(1,"MedicineName1", 30,20);
-        MedicineData m2 = new MedicineData(2,"MedicineName2", 5,20);
-        addMedicine(m1);
-        addMedicine(m2);
-
-//        MedicineRequest request = new MedicineRequest(1001,001,"APT0000001");
-//        requestMedicine(request);
-
-    }
-
-    public void presetData() {
         MedicineData m1 = new MedicineData(1,"Paracetamol", 100,20);
         MedicineData m2 = new MedicineData(2,"Ibuprofen", 50,10);
         MedicineData m3 = new MedicineData(3,"Amoxicillin", 75,15);
@@ -71,11 +96,19 @@ public class Pharmacy {
         addMedicine(m3);
     }
 
+    /**
+     * Adds a {@code MedicineRequest} into the queue
+     * @param request request to be added
+     */
     public void requestMedicine(MedicineRequest request) {
         this.medicineRequests.add(request);
         Input.ScanString("Request has been added. \nPress enter to continue...");
     }
 
+    /**
+     * Adds a {@code RestockRequest} into the queue
+     * @param request request to be added
+     */
     public void requestRestock(RestockRequest request) {
         if (request == null) return;
         updatePendingRestock(true , request);
@@ -83,6 +116,13 @@ public class Pharmacy {
         Input.ScanString("Request has been added. \nPress enter to continue...");
     }
 
+    /**
+     * Approves the {@code MedicineRequest} with the given {@code index}
+     * <p>
+     *     Moves request from {@code medicineRequest} (active list) to {@code pastMedReq} (completed list) for tracking
+     * </p>
+     * @param index {@code index} of the {@code MedicineRequest} in the ArrayList
+     */
     public void approveMedicine(int index) {
         MedicineRequest request = this.medicineRequests.remove(index);
         this.pastMedReq.add(request);
@@ -90,6 +130,13 @@ public class Pharmacy {
         Input.ScanString("Request has been approved. \nPress enter to continue...");
     }
 
+    /**
+     * Approves the {@code RestockRequest} with the given {@code index}
+     * <p>
+     *     Moves request from {@code restockRequest} (active list) to {@code pastRestockReq} (completed list) for tracking
+     * </p>
+     * @param index {@code index} of the {@code RestockRequest} in the ArrayList
+     */
     public void approveRestock (int index) {
         RestockRequest request = this.restockRequests.remove(index);
         updatePendingRestock(false , request);
@@ -99,6 +146,10 @@ public class Pharmacy {
         Input.ScanString("Request has been approved. \nPress enter to continue...");
     }
 
+    /**
+     * Prints out every {@code MedicineRequest} in {@code medicineRequest} (active list)
+     * @return the size of the ArrayList {@code medicineRequest}
+     */
     public int viewMedRequest() {
         int index = 1;
         for (MedicineRequest o : this.medicineRequests) {
@@ -107,6 +158,10 @@ public class Pharmacy {
         return this.medicineRequests.size();
     }
 
+    /**
+     * Prints out every {@code RestockRequest} in {@code restockRequests} (active list)
+     * @return the size of the ArrayList {@code restockRequest}
+     */
     public int viewRestockRequest() {
         int index = 1;
         for (RestockRequest o : this.restockRequests) {
@@ -115,20 +170,43 @@ public class Pharmacy {
         return this.restockRequests.size();
     }
 
+    /**
+     * Gets the request in the ArrayList {@code medicineRequest} with the given {@code index}
+     * @param index {@code index} of the {@code MedicineRequest}
+     * @return {@code null} if empty or out of bound, {@code MedicineRequest} when it is in the ArrayList
+     */
     public MedicineRequest getMedRequest(int index) {
-        if (this.medicineRequests.isEmpty()) return null;
+        if (this.medicineRequests.isEmpty() || index >= this.medicineRequests.size()) return null;
         return this.medicineRequests.get(index);
     }
 
+    /**
+     * Gets the request in the ArrayList {@code restockRequest} with the given {@code index}
+     * @param index {@code index} of the {@code RestockRequest}
+     * @return {@code null} if empty or out of bound, {@code RestockRequest} when it is in the ArrayList
+     */
     public RestockRequest getRestockRequest(int index) {
+        if (this.restockRequests.isEmpty() || index >= this.restockRequests.size()) return null;
         return this.restockRequests.get(index);
     }
 
+    /**
+     * Checks whether the {@code Pharmacy} has enough medicine to fulfill the request
+     * @param medicineName name of medicine to prescribe
+     * @param amount amount to be prescribed
+     * @return {@code true} when there is enough stock, {@code false} otherwise
+     */
     public boolean checkFulfillable(String medicineName, int amount){
         int medicineID = convertNameToID(medicineName);
         return medicineStorage.get(medicineID).amount > amount;
     }
 
+    /**
+     * Prescribes the medicine by deducting given {@code amount} from {@code medicineName} in the {@code medicineStorage}
+     * @param medicineName name of medicine
+     * @param amount amount of that medicine
+     * @return
+     */
     public boolean prescribeMedicine(String medicineName, int amount){
         int medicineID = convertNameToID(medicineName);
         if (medicineStorage.get(medicineID).amount < amount) return false;
@@ -136,6 +214,9 @@ public class Pharmacy {
         return true;
     }
 
+    /**
+     * Prints out the stock of the medicine in {@code medicineStorage} with its pending amount in {@code pendingAmount}
+     */
     public void viewStock() {
         System.out.println("_____________________________________________");
         for (Map.Entry<Integer, MedicineData> o: medicineStorage.entrySet()) {
@@ -150,6 +231,13 @@ public class Pharmacy {
         }
     }
 
+    /**
+     * Prints out the stock of the medicine in {@code medicineStorage} with its pending amount in {@code pendingAmount}
+     * <p>
+     *     Takes in a {@code HashMap<Integer,Integer>} storing the indenting amount which is to be printed
+     * </p>
+     * @param currentRequest {@code HashMap<Integer,Integer>} storing the indenting amount
+     */
     public void viewStock(HashMap<Integer,Integer> currentRequest) {
         System.out.println("_____________________________________________");
         for (Map.Entry<Integer, MedicineData> o: medicineStorage.entrySet()) {
@@ -166,6 +254,11 @@ public class Pharmacy {
         }
     }
 
+    /**
+     * Updates the {@code  HashMap<Integer,Integer> pendingAmount} when there is a new indent or a restock has been fulfilled
+     * @param Add {@code true} adds to the pending amount, {@code false} subtracts from the pending amount
+     * @param request {@code RestockRequest} to be referenced to
+     */
     public void updatePendingRestock( boolean Add , RestockRequest request ) {
         for (Map.Entry<Integer, Integer> o: request.getRequestAmount().entrySet()) {
             int ID = o.getKey();
@@ -174,16 +267,30 @@ public class Pharmacy {
         }
     }
 
+    /**
+     * Checks whether there is a medicine with the existing {@code ID}
+     * @param ID ID to be checked
+     * @return {@code true} if there is a medicine with that ID, {@code false} otherwise
+     */
     public boolean checkExistingID(int ID) {
         return medicineStorage.containsKey(ID);
     }
 
+    /**
+     * Takes in a medicine name and converts it into its ID by looking at the {@code HashMap<String,Integer> nameToID}
+     * @param medicineName the name of the mdicine
+     * @return the ID with that medicine name, {@code null} if no medicine with that name in the {@code Pharmacy}
+     */
     public int convertNameToID(String medicineName){
         medicineName = medicineName.toLowerCase();
         //System.out.println(medicineName);
         return nameToID.get(medicineName);
     }
 
+    /**
+     * Called when a {@code RestockRequest} is approved, and increase the stock with the information in the request
+     * @param request {@code RestockRequest} to be referenced
+     */
     private void updateCurrentStock(RestockRequest request) {
         for (Map.Entry<Integer,Integer> o: request.getRequestAmount().entrySet()) {
             int ID = o.getKey();
@@ -192,6 +299,11 @@ public class Pharmacy {
         }
     }
 
+    /**
+     * Adds a new medicine into the {@code Pharmacy}
+     * @param medicine medicine to be added
+     * @return {@code true} when successfully added, {@code false} otherwise
+     */
     private boolean addMedicine(MedicineData medicine) {
         if (medicineStorage.containsKey(medicine.ID)) return false; // Return false when there is same ID
         medicineStorage.put(medicine.ID,medicine);
@@ -200,6 +312,12 @@ public class Pharmacy {
         return true;
     }
 
+    /**
+     * Used to read and load in data from the file.
+     * <p>Creates a new {@code File} and {@code Scanner} with the file name {@code pharmacyFileName} and passes it into
+     * the function {@code loadData()}</p>
+     * @see #loadData(Scanner) loadData() function
+     */
     public void loadFile() {
         File savefile = new File(pharmacyFileName);
         Scanner file;
@@ -215,6 +333,10 @@ public class Pharmacy {
         }
     }
 
+    /**
+     * Reads from the given {@code Scanner} object and
+     * @param fileReader
+     */
     private void loadData(Scanner fileReader) {
         while (fileReader.hasNextLine()) {
             String text = fileReader.nextLine();
