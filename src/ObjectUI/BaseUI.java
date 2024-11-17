b8ff3d9b0c494956addda69c1fcee17906b269ec
Mitoz08 <timong22@yahoo.com>
2024-11-17 22:08:26 +0800

    Added 1. JavaDoc for BaseUI

--- src/ObjectUI/BaseUI.java
+++ src/ObjectUI/BaseUI.java
@@ -4,7 +4,11 @@ import DepartmentObject.Pharmacy;
 import DepartmentObject.UserInfoDatabase;
 import Singleton.ServerHMS;
 
+/** A Base Interface to keep the default {@code Variables} and {@code Methods} a User Interface should have*/
 public interface BaseUI {
+    /** {@code UserInfoData} object used by all the User Interfaces*/
     public UserInfoDatabase database = ServerHMS.getInstance().getDatabase();
+
+    /** {@code Pharmacy} object used by all the User Interfaces*/
     public Pharmacy pharmacy = ServerHMS.getInstance().getPharmacy();
 }
